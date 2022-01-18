ad84e30019e2d50f33b8d978bc6ef04b325eeb78
miruoon <rillakerika@gmail.com>
2022-01-19 07:58:32 +0900

    api取得を別ファイルへ移行

--- lib/axios.ts
+++ lib/axios.ts
@@ -7,39 +7,32 @@ const PRIVATE_KEY = process.env['MARVEL_PRIVATE_KEY'] as string
 // TODO: refactor
 
 const API_BASE_URL = 'https://gateway.marvel.com/v1/public/comics'
-
-// export const client = axios.create({
-//   baseURL: 'https://gateway.marvel.com/v1/public/comics',
-// })
+const DISPLAY_COMICS_LIMIT = 20
 
 export const callMarvelApi = async (params: Record<string, string>) => {
   const ts = Date.now().toString()
   const paramsWithKey = {
     ...params,
-    apiKey: PUBLIC_KEY,
+    apikey: PUBLIC_KEY,
     hash: md5(ts + PRIVATE_KEY + PUBLIC_KEY).toString(),
     ts,
   }
   const searchParamsWithKey = new URLSearchParams(paramsWithKey).toString()
-  // const { data } = await client.get(`?q=${searchParamsWithKey}`)
   const url = new URL(API_BASE_URL)
   url.search = searchParamsWithKey
-  const { data } = await axios.get(
-    'https://jsonplaceholder.typicode.com/todos/1',
-  )
-  // const { data } = await axios.get(`${API_BASE_URL}?q=${searchParamsWithKey}`)
+  const { data } = await axios.get(url.toString())
   return data
 }
 
 const getComicsSearchParams = (
-  year: number,
+  year: string,
   _offset: string,
   limit: string,
 ) => {
   return {
     formatType: 'comic',
     noVariants: 'true',
-    // dateRange: `${year}-01-01,${year}-12-31`,
+    dateRange: `${year}-01-01,${year}-12-31`,
     hasDigitalIssue: 'true',
     limit,
     // offset,
@@ -47,7 +40,9 @@ const getComicsSearchParams = (
   }
 }
 
-export const getComics = async () => {
-  const data = await callMarvelApi(getComicsSearchParams(2021, '2021', '10'))
+export const getComics = async (year: string) => {
+  const data = await callMarvelApi(
+    getComicsSearchParams(year, year, DISPLAY_COMICS_LIMIT.toString()),
+  )
   return data
 }
--- pages/year/[year].tsx
+++ pages/year/[year].tsx
@@ -1,8 +1,6 @@
-// import { getComics } from 'lib/axios'
-import md5 from 'crypto-js/md5.js'
 import { GetServerSideProps } from 'next'
-import axios from 'axios'
 import Image from 'next/image'
+import { getComics } from 'lib/axios'
 
 // TODO: 型定義＆api取得部分を外部ファイルへ
 
@@ -13,7 +11,7 @@ interface YearPageProps {
 
 const YearPage = ({ data }: YearPageProps) => {
   return (
-    <div className="grid grid-cols-4">
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
       {data.map((d) => (
         <div key={d.id} className="">
           <a href={d.urls[0].url} target="_blank" rel="noreferrer">
@@ -38,45 +36,8 @@ const YearPage = ({ data }: YearPageProps) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const PUBLIC_KEY = process.env['MARVEL_PUBLIC_KEY'] as string
-  const PRIVATE_KEY = process.env['MARVEL_PRIVATE_KEY'] as string
-
-  const API_BASE_URL = 'https://gateway.marvel.com:443/v1/public/comics'
-
-  const callMarvelApi = async (params: Record<string, string>) => {
-    // const ts = Date.now().toString()
-    // const ts = new Date().getTime().toString()
-    const paramsWithKey = {
-      ...params,
-      // apiKey: PUBLIC_KEY,
-      // hash: md5(ts + PRIVATE_KEY + PUBLIC_KEY).toString(),
-      // ts,
-    }
-    const searchParamsWithKey = new URLSearchParams(paramsWithKey).toString()
-    const url = new URL(API_BASE_URL)
-    url.search = searchParamsWithKey
-    const { data } = await axios.get(url.toString())
-    return data
-  }
-
-  const getComicsSearchParams = (year: string, limit: string) => {
-    const ts = new Date().getTime().toString()
-    return {
-      ts,
-      apikey: PUBLIC_KEY,
-      hash: md5(ts + PRIVATE_KEY + PUBLIC_KEY).toString(),
-      // formatType: 'comic',
-      // noVariants: 'true',
-      dateRange: `${year}-01-01,${year}-12-31`,
-      // hasDigitalIssue: 'true',
-      limit,
-      // offset,
-      // orderBy: 'modified',
-    }
-  }
-
   const year = context.params?.year as string
-  const { data } = await callMarvelApi(getComicsSearchParams(year, '10'))
+  const { data } = await getComics(year)
 
   if (!data) {
     return {
