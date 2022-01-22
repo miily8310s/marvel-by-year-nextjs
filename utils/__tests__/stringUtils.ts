2b021db735d93980d13724cda0228f49140dbacd
miruoon <rillakerika@gmail.com>
2022-01-22 18:56:07 +0900

    stringUtilsのユニットテストの作成を完了

--- utils/__tests__/stringUtils.ts
+++ utils/__tests__/stringUtils.ts
@@ -7,11 +7,21 @@ import {
 describe('stringUtils', () => {
   test('getPageTitle', () => {
     expect(getPageTitle()).toBe('Home | Marvel Unlimited by Year')
+    expect(getPageTitle('Test Comic')).toBe(
+      'Test Comic | Marvel Unlimited by Year',
+    )
   })
   test('getArrayToLimitedText', () => {
     expect(getArrayToLimitedText([], 1)).toBe('Unknown')
+    expect(getArrayToLimitedText(['test'], 1)).toBe('test')
+    expect(getArrayToLimitedText(['here', 'now'], 2)).toBe('here and now')
+    expect(getArrayToLimitedText(['one', 'two', 'three', 'four'], 3)).toBe(
+      'one, two, three and 1 others',
+    )
   })
   test('commaSeparate', () => {
     expect(commaSeparate([])).toBe(' and undefined')
+    expect(commaSeparate(['hoge', 'fuga'])).toBe('hoge and fuga')
+    expect(commaSeparate(['1', '2', '3'])).toBe('1, 2 and 3')
   })
 })
--- utils/yearsUtils.ts
+++ utils/yearsUtils.ts
@@ -1,6 +1,9 @@
 export const MIN_PUBLISH_YEAR = 1939
 export const MAX_PUBLISH_YEAR = new Date().getFullYear() + 1
 
+/**
+ * MIN_PUBLISH_YEARから今年の年を配列で返す
+ */
 export const publishYears = Array.from(
   Array(MAX_PUBLISH_YEAR - MIN_PUBLISH_YEAR).keys(),
 ).map((year) => year + MIN_PUBLISH_YEAR)
